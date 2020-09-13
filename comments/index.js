a8b6fa789882a784fa6d2bac19e6362bdbcf9244
kielkow <matheuskiel@fiorifer.com.br>
2020-09-13 16:00:41 -0300

    Emitting Comments Events

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 const { randomBytes } = require('crypto');
 
 const app = express();
@@ -17,7 +18,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.status(201).send(commentsByPostId[id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
 
     const { id } = req.params;
@@ -31,6 +32,15 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content });
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId, 
+            content,
+            postId: req.params.id
+        }
+    });
 
     res.status(201).send(comments);
 });
