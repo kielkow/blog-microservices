33871645c0df1c9bd6e42a8909fdc8788e469390
kielkow <matheuskiel@fiorifer.com.br>
2020-09-15 14:18:46 -0300

    Implementing Event Sync

--- query/index.js
+++ query/index.js
@@ -9,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -30,7 +24,7 @@ app.post('/events', (req, res) => {
         post.comments.push({ id, content, status });
     }
 
-    if (type === 'CommentCreated') {
+    if (type === 'CommentUpdated') {
         const { id, postId, content, status } = data;
 
         const post = posts[postId];
@@ -41,10 +35,28 @@ app.post('/events', (req, res) => {
 
         comment.content = content;
     }
+};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.status(200).send({ status: 200, message: 'Event receveid and processed' });
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002');
+
+    const { data } = await axios.get('http://localhost:4005/events');
+
+    for (const event of data) {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
